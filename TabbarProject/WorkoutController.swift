5cfe823e70f9e22a03ea6596c971e69b0a0e484d
Ruchira Bandara <38920827+RuchiraHansanaBandara@users.noreply.github.com>
2024-06-24 07:38:38 +0530

    Update WorkoutController.swift

--- TabbarProject/WorkoutController.swift
+++ TabbarProject/WorkoutController.swift
@@ -14,4 +14,14 @@ class WorkoutController: UIViewController {
         self.view.backgroundColor = .systemPurple
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
 }
+
